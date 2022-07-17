4dd85f562ef703ef150ac8b4367e73dfcb99acf0
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2022-07-17 18:02:08 -0400

    add comments

--- DirDiff/DirMetaSnapshotReaders/IDirMetaSnapshotReader.cs
+++ DirDiff/DirMetaSnapshotReaders/IDirMetaSnapshotReader.cs
@@ -4,9 +4,22 @@ namespace DirDiff.DirMetaSnapshotReaders;
 
 public interface IDirMetaSnapshotReader
 {
+    /// <summary>
+    /// Snapshot reader options.
+    /// </summary>
     DirMetaSnapshotReaderOptions Options { get; }
 
+    /// <summary>
+    /// Configure snapshot reader options.
+    /// </summary>
+    /// <param name="action">Configure action.</param>
+    /// <returns></returns>
     IDirMetaSnapshotReader Configure(Action<DirMetaSnapshotReaderOptions> action);
 
+    /// <summary>
+    /// Reads snapshot from stream.
+    /// </summary>
+    /// <param name="stream">Stream.</param>
+    /// <returns>Snapshot.</returns>
     Task<DirMetaSnapshot> ReadAsync(Stream stream);
 }
